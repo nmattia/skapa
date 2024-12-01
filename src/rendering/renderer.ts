13fb53442089ac140dce220f433587113f0abd1c
Nicolas Mattia <nicolas@nmattia.com>
2024-12-01 17:45:39 +0100

    Style range inputs

--- src/rendering/renderer.ts
+++ src/rendering/renderer.ts
@@ -180,7 +180,7 @@ export class Renderer {
     const height = top - bottom;
     const maxDim = Math.sqrt(width * width + height * height);
     // The camera was moved/updated, so recompute the thickness of the outline
-    this.thickenPass.setThickness((50 * window.devicePixelRatio) / maxDim);
+    this.thickenPass.setThickness((100 * window.devicePixelRatio) / maxDim);
   }
 
   render() {
